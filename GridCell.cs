a51f0edff18f27366ed1f863a216a29ff1661253
ciresimo <149970685+ciresimo@users.noreply.github.com>
2023-11-05 22:23:41 +0200

    Latest version of files

---
+++ GridCell.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridCell {   
+    private int gCost;
+    private int hCost;
+    private int fCost;
+
+    //to trace back the path to take
+    public GridCell cameFromCell;
+
+    private int x;
+    private int y;
+
+    public bool isOccupied = false; 
+
+    public GridCell(int x, int y){
+        this.x = x;
+        this.y = y;
+        gCost=0;
+    }
+
+    // Set the position of this grid cell on the grid
+    public void SetPosition( int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    
+    //get the position of this grid space on the grid
+    public Vector2Int GetPosition(){
+        return new Vector2Int(x, y);
+    }
+
+    public void SetGCost(int gCost){
+        this.gCost = gCost;
+    }
+
+    public int GetGCost(){
+        return gCost;
+    }
+
+    public void SetHCost(int hCost){
+        this.hCost = hCost;
+    }
+    public void SetFCost(int fCost){
+        this.fCost = fCost;
+    }
+
+    public int GetFCost(){
+        return fCost;
+    }
+
+    public void CalculateFCost(){
+        fCost = gCost+hCost;
+    }
+
+    //ToString Method
+    public override string ToString()
+    {
+        return x +"," +y +"-"+gCost+"-"+fCost+"-"+hCost;
+    }
+
+    // override object.Equals
+    public override bool Equals(object obj)
+    {       
+        if (obj == null || GetType() != obj.GetType())
+        {
+            return false;
+        }
+        
+        if (!(obj is GridCell))
+        {
+            return false;
+        }
+        
+        return (this.x == ((GridCell)obj).x)
+                && (this.y == ((GridCell)obj).y);
+    }
+    
+    // override object.GetHashCode
+    public override int GetHashCode()
+    {
+        return 35^x+y;
+    }
+    
+}
